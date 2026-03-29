3a316cc30fc0ffd3705e77d707ef636ca76b92f8
Conficius Adda <addawebadua@gmail.com>
2026-03-29 13:57:04 +0000

    feat: add fake access service for better-auth CLI configuration

--- better-auth.config.ts
+++ better-auth.config.ts
@@ -8,15 +8,16 @@ import { betterAuth } from "better-auth";
 import { DB } from "./src/lib/db";
 import { getAuthOptions } from "@/lib/auth.config";
 import { AuthConfigEnvVar } from "@/lib/get-env";
+import { AccessService } from "@/services/access/access.service";
 
 // When running better-auth CLI (whether locally or in CI environment), we use a fake D1 database instance ...
 // ... since we don't have a real D1 database outside of a Cloudflare Workers runtime context.
 const fakeDb = Object.create(null) as unknown as DB;
 const fakeAuthConfig = Object.create({}) as AuthConfigEnvVar; // No auth config for CLI invocation
-
+const fakeAccessService = Object.create({}) as unknown as AccessService; // No access service for CLI invocation
 const authConfig = getAuthOptions({
   db: fakeDb,
-
+  accessService: fakeAccessService,
   config: {
     ...fakeAuthConfig,
     secret: "this-is-a-fake-secret-only-used-for-cli-invocation",
